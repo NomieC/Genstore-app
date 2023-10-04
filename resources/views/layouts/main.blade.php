e4b48da0bd2f68c4d630f60d21ff9c9ee39d996d
NomieC <Fbdarykids@gmail.com>
2023-10-05 00:48:25 +0700

    changes title and navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{ $title }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="h-14 bg-gradient-to-r from-stone-500 to-gray-700">
+    
     @include('component.navbar')
-    <div >
+    
+    <div>
         @yield('container')
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 Route::get('/about', function () {
-    return view('about');
+    return view('about', [
+        "title" => "About us"
+    ]);
 });
