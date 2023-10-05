3b7512e2181ec2f79259522538b5c7f51cee2955
NomieC <Fbdarykids@gmail.com>
2023-10-05 11:24:57 +0700

    done

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>about</h1>
+<header class="mt-10">
+    <h1 class="text-9xl text-gray-300 ml-20">Bon apetit</h1>
+    <div class="ml-20 flex text-center">
+        @include('component.buttonkeren')
+    </div>
+</header>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 <header class="mt-10">
     <h1 class="text-9xl text-gray-300 ml-20">Bon apetit</h1>
-    <div class="mt-8 flex text-center">
+    <div class="ml-20 flex text-center">
         @include('component.buttonkeren')
     </div>
 </header>
