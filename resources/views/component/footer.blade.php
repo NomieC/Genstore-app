435bc32a6c8d4318f70121f4dcea33e049f8d68e
NomieC <Fbdarykids@gmail.com>
2023-10-07 19:56:42 +0700

    --

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -2,12 +2,11 @@
     <div class="mx-auto max-w-screen-xl px-4 pb-8 pt-16 sm:px-6 lg:px-8 lg:pt-24">
         <div class="text-center">
             <h2 class="text-3xl font-extrabold text-gray-900 sm:text-5xl">
-                Customise Your Product
+            Maison de l'Anglais
             </h2>
 
             <p class="mx-auto mt-4 max-w-sm text-gray-500">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum maiores
-                ipsum eos temporibus ea nihil.
+                We live, We love, Woi lah
             </p>
 
             <div class="mt-8 inline-block">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.main')
 
 @section('container')
-<header class="h-screen" style="background-color: #bcb7a4;">
-    <h1 class="text-9xl ml-20">Maison de l'Anglais</h1>
+<header class="h-screen flex justify-center items-center" style="background-color: #bcb7a4;">
+    <h1 class="text-9xl">Maison de l'Anglais</h1>
 </header>
+
 @endsection
